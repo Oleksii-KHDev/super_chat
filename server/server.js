f5ef98feb1af902319c5a38a4c7097f4d16ed6ef
Oleksii-KHDev <alexeyzolotuhin@gmail.com>
2023-03-14 16:52:46 +0200

    add sockets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const path = require('node:path');
 const express = require('express');
-const app = express();
+const http = require('node:http');
+const { Server } = require('socket.io');
 
-// eslint-disable-next-line no-unused-vars
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const app = express();
+app.use(express.static(publicPath));
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connection');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server has been started on port: ${port}`);
 });
