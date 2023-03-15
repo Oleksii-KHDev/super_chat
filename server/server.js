ef5984ee5fa55f1cf2a2ab525be8ae36cc8051f7
Oleksii-KHDev <alexeyzolotuhin@gmail.com>
2023-03-15 06:37:07 +0200

    Rename public to client

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('node:http');
 const { Server } = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../client');
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.static(publicPath));
