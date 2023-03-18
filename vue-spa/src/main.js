724928faf38dec4fd512bc30134e3430200a0f5d
Oleksii-KHDev <alexeyzolotuhin@gmail.com>
2023-03-18 08:41:13 +0200

    Add routes, prettify vue code

--- vue-spa/src/main.js
+++ vue-spa/src/main.js
@@ -1,15 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  created: function () {
+    this.checkLogin();
+  },
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+  methods: {
+    checkLogin() {
+      if (!localStorage.getItem('login')) {
+        this.$router.push('/login');
+      } else {
+        this.$router.push('/chat');
+      }
+    },
+  },
+});
--- vue-spa/src/router/index.js
+++ vue-spa/src/router/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/components/Login';
+import Register from '@/components/Register';
+import Chat from '@/components/Chat';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+    },
+  ],
+});
